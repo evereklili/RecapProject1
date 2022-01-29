15e8e91036371124505b36f8da4090662a227f1f
OMER DOGU - www.omerdogu.com <omerdogu@omerdogu.com>
2022-01-29 12:19:41 +0300

    Add project files.

---
+++ RecapProject1/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace RecapProject1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gbxCategory = new System.Windows.Forms.GroupBox();
+            this.dgwProduct = new System.Windows.Forms.DataGridView();
+            this.gbxProductName = new System.Windows.Forms.GroupBox();
+            this.lblSearch = new System.Windows.Forms.Label();
+            this.tbxSearch = new System.Windows.Forms.ComboBox();
+            this.lblCategory = new System.Windows.Forms.Label();
+            this.cbxCategory = new System.Windows.Forms.ComboBox();
+            this.gbxCategory.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgwProduct)).BeginInit();
+            this.gbxProductName.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // gbxCategory
+            // 
+            this.gbxCategory.Controls.Add(this.dgwProduct);
+            this.gbxCategory.Controls.Add(this.gbxProductName);
+            this.gbxCategory.Controls.Add(this.lblCategory);
+            this.gbxCategory.Controls.Add(this.cbxCategory);
+            this.gbxCategory.Location = new System.Drawing.Point(12, 21);
+            this.gbxCategory.Name = "gbxCategory";
+            this.gbxCategory.Size = new System.Drawing.Size(721, 533);
+            this.gbxCategory.TabIndex = 0;
+            this.gbxCategory.TabStop = false;
+            this.gbxCategory.Text = "Kategoriye Göre Listele";
+            this.gbxCategory.Enter += new System.EventHandler(this.gbxCategory_Enter);
+            // 
+            // dgwProduct
+            // 
+            this.dgwProduct.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgwProduct.Location = new System.Drawing.Point(18, 184);
+            this.dgwProduct.Name = "dgwProduct";
+            this.dgwProduct.Size = new System.Drawing.Size(642, 320);
+            this.dgwProduct.TabIndex = 3;
+            // 
+            // gbxProductName
+            // 
+            this.gbxProductName.Controls.Add(this.lblSearch);
+            this.gbxProductName.Controls.Add(this.tbxSearch);
+            this.gbxProductName.Location = new System.Drawing.Point(18, 84);
+            this.gbxProductName.Name = "gbxProductName";
+            this.gbxProductName.Size = new System.Drawing.Size(637, 85);
+            this.gbxProductName.TabIndex = 2;
+            this.gbxProductName.TabStop = false;
+            this.gbxProductName.Text = "Ürün İsmine Göre Ara";
+            // 
+            // lblSearch
+            // 
+            this.lblSearch.AutoSize = true;
+            this.lblSearch.Location = new System.Drawing.Point(38, 27);
+            this.lblSearch.Name = "lblSearch";
+            this.lblSearch.Size = new System.Drawing.Size(23, 13);
+            this.lblSearch.TabIndex = 4;
+            this.lblSearch.Text = "Ara";
+            // 
+            // tbxSearch
+            // 
+            this.tbxSearch.FormattingEnabled = true;
+            this.tbxSearch.Location = new System.Drawing.Point(125, 24);
+            this.tbxSearch.Name = "tbxSearch";
+            this.tbxSearch.Size = new System.Drawing.Size(121, 21);
+            this.tbxSearch.TabIndex = 3;
+            // 
+            // lblCategory
+            // 
+            this.lblCategory.AutoSize = true;
+            this.lblCategory.Location = new System.Drawing.Point(44, 32);
+            this.lblCategory.Name = "lblCategory";
+            this.lblCategory.Size = new System.Drawing.Size(46, 13);
+            this.lblCategory.TabIndex = 1;
+            this.lblCategory.Text = "Kategori";
+            // 
+            // cbxCategory
+            // 
+            this.cbxCategory.FormattingEnabled = true;
+            this.cbxCategory.Location = new System.Drawing.Point(143, 29);
+            this.cbxCategory.Name = "cbxCategory";
+            this.cbxCategory.Size = new System.Drawing.Size(121, 21);
+            this.cbxCategory.TabIndex = 0;
+            this.cbxCategory.SelectedIndexChanged += new System.EventHandler(this.cbxCategory_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(820, 559);
+            this.Controls.Add(this.gbxCategory);
+            this.Name = "Form1";
+            this.Text = "Ürünler";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.gbxCategory.ResumeLayout(false);
+            this.gbxCategory.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgwProduct)).EndInit();
+            this.gbxProductName.ResumeLayout(false);
+            this.gbxProductName.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox gbxCategory;
+        private System.Windows.Forms.DataGridView dgwProduct;
+        private System.Windows.Forms.GroupBox gbxProductName;
+        private System.Windows.Forms.Label lblSearch;
+        private System.Windows.Forms.ComboBox tbxSearch;
+        private System.Windows.Forms.Label lblCategory;
+        private System.Windows.Forms.ComboBox cbxCategory;
+    }
+}
+
---
+++ RecapProject1/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace RecapProject1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Load eventine gideriz. bu bir eventter. biz ona kayıt oluyoruz 
+            //orname dapar, adonet kütüphanesi, kullanılsa  da ben entityframework kullanacağım. 
+            ListCategories();
+            NewMethod();
+        }
+
+        private void NewMethod()
+        {
+            using (NorthwindContext context = new NorthwindContext())
+            {
+                dgwProduct.DataSource = context.Products.ToList();
+
+            }
+        }
+        private void ListCategories()
+        {
+            using (NorthwindContext context = new NorthwindContext())
+            {
+
+                cbxCategory.DataSource = context.Categories.ToList();
+                cbxCategory.DisplayMember = "CategoryName";
+                cbxCategory.ValueMember = "CategoryId";
+
+            }
+        }
+
+        private void ListProducsByCategory(int categoryId)
+        {
+            using (NorthwindContext context = new NorthwindContext())
+            {
+                dgwProduct.DataSource = context.Products.Where(p => p.CategoryId == categoryId).ToString();
+            }
+        }
+
+        private void gbxCategory_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cbxCategory_SelectedIndexChanged(object sender, EventArgs e)
+        {
+           try
+            {
+
+                ListProducsByCategory(Convert.ToInt16(cbxCategory.SelectedValue));
+            }
+            catch
+            {
+
+            }
+        }
+    }
+}
